dd0d32347ad1556baeed60981824498f3d82146b
Gerry Gastelum <gerry.gastelum.g@gmail.com>
2025-01-05 12:14:01 -0700

    Refactor examples

--- examples/Lookups/Program.cs
+++ examples/Lookups/Program.cs
@@ -1,4 +1,5 @@
-﻿using Lookups.TrieExamples;
+﻿using AdvancedDataStructures.Lookups;
+using Lookups.TrieExamples;
 
 // *** Trie Examples ***
 Console.WriteLine("*** Trie examples ***");
@@ -6,10 +7,4 @@ Console.WriteLine("*** Trie examples ***");
 // Autocomplete
 Console.WriteLine("Running autocomplete example...");
 var result = Autocomplete.Result(["apple", "app", "apricot", "banana", "bat", "batch"], "ap");
-
-// Print the results
-Console.WriteLine("Autocomplete suggestions for 'ap':");
-foreach (string word in result)
-{
-    Console.WriteLine(word);
-}
+Console.WriteLine($"Autocomplete suggestions for 'ap': {String.Join(", ", result)}");
--- examples/Lookups/TrieExamples/Autocomplete.cs
+++ examples/Lookups/TrieExamples/Autocomplete.cs
@@ -8,23 +8,5 @@ public static class Autocomplete
     {
         var trie = new Trie(words);
         return trie.QueryWords(prefix);
-        // // Insert words into the Trie
-        // trie.Insert("apple");
-        // trie.Insert("app");
-        // trie.Insert("apricot");
-        // trie.Insert("banana");
-        // trie.Insert("bat");
-        // trie.Insert("batch");
-
-        // Find all words with the prefix "ap"
-        
-        //var autocompleteResults = trie.QueryWords(prefix);
-
-        // // Print the results
-        // Console.WriteLine($"Autocomplete suggestions for '{prefix}':");
-        // foreach (var word in autocompleteResults)
-        // {
-        //     Console.WriteLine(word);
-        // }
     }
 }
